dd8e7b0f563dda82275d4d8d68a366a82cc7d40a
M-Saad-0 <70463206+M-Saad-0@users.noreply.github.com>
2022-12-15 20:10:43 +0500

    Update activity_6_4_1.cpp

--- activity_6_4_1.cpp
+++ activity_6_4_1.cpp
@@ -1,5 +1,44 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 class First{
-
+    protected:
+        int f;
+    public:
+        void f_input()
+        {
+            cout<<"Enter first integer: ";
+            cin>>f;
+        }
 };
+class Second: public First{
+    protected:
+        int s;
+    public:
+        void s_input()
+        {
+            f_input();
+            cout<<"Enter second integer: ";
+            cin>>s;
+        }
+};
+class Third: public Second{
+    protected:
+        int t;
+    public:
+        void t_input()
+        {
+            s_input();
+            cout<<"Enter third integer: ";
+            cin>>t;
+        }
+        int dispMax()
+        {
+            return f>s && f>t? f: s>f && s>t? s: t;
+        }
+};
+int main()
+{
+    Third th;
+    th.t_input();
+    cout<<"The greatest number is: "<<th.dispMax()<<endl;
+}
